32ea3cee988fe698b95302810563509a0cee8233
Olga-Kulagina <olga.kulagina17@yandex.ru>
2020-09-16 21:48:28 +0300

    Refactor Users to class component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -10,24 +10,27 @@ type UsersPropsType = {
     setUsers: (users: Array<UserType>) => void
 }
 
-const Users = (props: UsersPropsType) => {
-    if(props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
+class Users extends React.Component<UsersPropsType, any> {
+
+    componentDidMount() {
+        if(this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                this.props.setUsers(response.data.items)
+            })
+        }
     }
 
-    return (
-        <div>
+    render = () => {
+        return <div>
             {
-                props.users.map(u => <div key={u.id}>
+                this.props.users.map(u => <div key={u.id}>
                     <div>
                         <div>
                             <img src={u.photos.small != null ? u.photos.small : userPhoto} alt='avatar'/>
                         </div>
                         <div>
-                            {u.followed ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button> :
-                                <button onClick={() => props.follow(u.id)}>Follow</button>}
+                            {u.followed ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button> :
+                                <button onClick={() => this.props.follow(u.id)}>Follow</button>}
 
                         </div>
                     </div>
@@ -39,7 +42,7 @@ const Users = (props: UsersPropsType) => {
                 </div>)
             }
         </div>
-    )
+    }
 }
 
 export default Users;
