94295265af50a83e876f7061c935fff2f9abad18
Olga-Kulagina <olga.kulagina17@yandex.ru>
2020-10-03 22:27:03 +0300

    Add user subscription with put and delete requests

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,6 +3,7 @@ import {UserType} from '../../redux/users-reducer';
 import userPhoto from '../../assets/images/avatar.png'
 import s from './Users.module.css'
 import {NavLink} from 'react-router-dom';
+import axios from 'axios';
 
 type UsersPropsType = {
     users: Array<UserType>
@@ -46,8 +47,34 @@ const Users = (props: UsersPropsType) => {
                             </NavLink>
                         </div>
                         <div>
-                            {u.followed ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button> :
-                                <button onClick={() => props.follow(u.id)}>Follow</button>}
+                            {u.followed
+                                ? <button onClick={() => {
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,{
+                                        withCredentials: true,
+                                        headers: {
+                                            'API-KEY': '99efda20-dfa9-469b-8832-45007a73919e'
+                                        }
+                                    }).then(response => {
+                                        if (response.data.resultCode === 0){
+                                            props.unfollow(u.id)
+                                        }
+                                    })
+
+                                }}>Unfollow</button> :
+                                <button onClick={() => {
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                        withCredentials: true,
+                                        headers: {
+                                            'API-KEY': '99efda20-dfa9-469b-8832-45007a73919e'
+                                        }
+                                    }).then(response => {
+                                        if (response.data.resultCode === 0){
+                                            props.follow(u.id)
+                                        }
+                                    })
+                                }
+
+                                }>Follow</button>}
 
                         </div>
                     </div>
