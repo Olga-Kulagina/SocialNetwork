8cdee4e5ea8726261ff79f8e6749a73c964790ab
Olga-Kulagina <olga.kulagina17@yandex.ru>
2020-10-04 10:14:37 +0300

    Add api directory, api.ts, usersAPI

---
+++ src/api/api.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': '99efda20-dfa9-469b-8832-45007a73919e'
+    }
+})
+
+export const usersAPI = {
+    getUsers (currentPage: number = 1, pageSize: number = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data)
+    }
+}
